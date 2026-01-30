c01a172dca6a997fa146187e9d538f6cc6f32c46
white-richard <rich.white285@yahoo.com>
2026-01-29 21:16:22 -0800

    fixed inaturalist imp to check for only updates to a dataset faster

--- wildlife_datasets/datasets/downloads.py
+++ wildlife_datasets/datasets/downloads.py
@@ -1,25 +1,27 @@
-import os
-import json 
-import time
-import shutil
 import datetime
-from typing import List, Optional, Tuple
+import json
+import os
+import shutil
+import time
 
-from datasets import load_dataset
 import pandas as pd
 from pyinaturalist import get_observations
+
+from datasets import load_dataset
+
 from . import utils
 
 
 def check_attribute(obj, attr):
     if not hasattr(obj, attr):
-        raise Exception(f'Object {obj} must have attribute {attr}.')
+        raise Exception(f"Object {obj} must have attribute {attr}.")
 
 
 def json_serial(obj):
     if isinstance(obj, (datetime.datetime, datetime.date)):
         return obj.isoformat()
     raise TypeError("Type not serializable")
+
 
 class DownloadURL:
     url = None
@@ -34,39 +36,40 @@ class DownloadURL:
             if cls.archive:
                 utils.download_url(cls.url, cls.archive)
             else:
-                raise ValueError('When cls.url is specified, cls.archive must also be specified')
+                raise ValueError("When cls.url is specified, cls.archive must also be specified")
         for url, archive in cls.downloads:
-            utils.download_url(url, archive)        
+            utils.download_url(url, archive)
 
     @classmethod
-    def _extract(cls, exts = ['.zip', '.tar', '.tar.gz', '.tgz', '.tar.bz2', '.rar', '.7z']):
+    def _extract(cls, exts=[".zip", ".tar", ".tar.gz", ".tgz", ".tar.bz2", ".rar", ".7z"]):
         if cls.archive:
             if any(cls.archive.endswith(ext) for ext in exts):
                 utils.extract_archive(cls.archive, delete=True)
         for _, archive in cls.downloads:
             if any(archive.endswith(ext) for ext in exts):
                 if cls.extract_add_folder:
-                    archive_name = archive.split('.')[0]
+                    archive_name = archive.split(".")[0]
                     utils.extract_archive(archive, extract_path=archive_name, delete=True)
                 else:
                     utils.extract_archive(archive, delete=True)
         if cls.rmtree:
             shutil.rmtree(cls.rmtree)
 
+
 class DownloadKaggle:
     @classmethod
     def _download(cls):
-        check_attribute(cls, 'kaggle_url')
-        check_attribute(cls, 'kaggle_type')
+        check_attribute(cls, "kaggle_url")
+        check_attribute(cls, "kaggle_type")
         display_name = cls.display_name().lower()
-        if cls.kaggle_type == 'datasets':
-            command = f'datasets download -d {cls.kaggle_url} --force'
-        elif cls.kaggle_type == 'competitions':
-            command = f'competitions download -c {cls.kaggle_url} --force'
+        if cls.kaggle_type == "datasets":
+            command = f"datasets download -d {cls.kaggle_url} --force"
+        elif cls.kaggle_type == "competitions":
+            command = f"competitions download -c {cls.kaggle_url} --force"
         else:
-            raise ValueError(f'cls.kaggle_type must be datasets or competitions.')
-        exception_text = f'''Kaggle must be setup.
-            Check https://wildlifedatasets.github.io/wildlife-datasets/downloads#{display_name}'''
+            raise ValueError("cls.kaggle_type must be datasets or competitions.")
+        exception_text = f"""Kaggle must be setup.
+            Check https://wildlifedatasets.github.io/wildlife-datasets/downloads#{display_name}"""
         try:
             os.system(f"kaggle {command}")
         except:
@@ -80,22 +83,23 @@ class DownloadKaggle:
         try:
             utils.extract_archive(cls.archive_name(), delete=True)
         except:
-            exception_text = f'''Extracting failed.
+            exception_text = f"""Extracting failed.
                 Either the download was not completed or the Kaggle terms were not agreed with.
-                Check https://wildlifedatasets.github.io/wildlife-datasets/downloads#{display_name}'''
+                Check https://wildlifedatasets.github.io/wildlife-datasets/downloads#{display_name}"""
             raise Exception(exception_text)
-    
+
     @classmethod
     def archive_name(cls):
-        return cls.kaggle_url.split('/')[-1] + '.zip'
-        
+        return cls.kaggle_url.split("/")[-1] + ".zip"
+
+
 class DownloadHuggingFace:
     determined_by_df = False
     saved_to_system_folder = True
 
     @classmethod
     def _download(cls, *args, **kwargs):
-        check_attribute(cls, 'hf_url')
+        check_attribute(cls, "hf_url")
         load_dataset(cls.hf_url, *args, **kwargs)
 
     @classmethod
@@ -116,15 +120,15 @@ class DownloadINaturalist:
       are downloaded; if None, all species are accepted.
     """
 
-    username: Optional[str] = None
-    project_id: Optional[int | str] = None
+    username: str | None = None
+    project_id: int | str | None = None
 
     per_page: int = 10
     delay: float = 1.0  # seconds between API pages
-    target_species_guess: Optional[List[str]] = None
+    target_species_guess: list[str] | None = None
 
     # Only a subset of metadata to keep things small
-    metadata_fields: Optional[Tuple[str, ...]] = None
+    metadata_fields: tuple[str, ...] | None = None
 
     @classmethod
     def _download(cls, force=None):
@@ -153,10 +157,12 @@ class DownloadINaturalist:
                 break
 
             for obs in observations["results"]:
-                if cls.target_species_guess is not None:
-                    if obs.get("species_guess", "") not in cls.target_species_guess:
-                        continue
-                
+                if (
+                    cls.target_species_guess is not None
+                    and obs.get("species_guess", "") not in cls.target_species_guess
+                ):
+                    continue
+
                 individual_id = obs.get("id")
                 for photo in obs.get("photos", []):
                     photo_id = photo["id"]
@@ -177,7 +183,7 @@ class DownloadINaturalist:
                     if not os.path.exists(file_name_image):
                         img = utils.download_image(url, file_name=file_name_image)
                         if not img:
-                            print(f'{url}: image download failed')
+                            print(f"{url}: image download failed")
                             continue
 
                     if cls.metadata_fields is None:
@@ -185,9 +191,7 @@ class DownloadINaturalist:
                             key: value for key, value in obs.items() if key != "photos"
                         }
                     else:
-                        selected_metadata = {
-                            key: obs.get(key) for key in cls.metadata_fields
-                        }
+                        selected_metadata = {key: obs.get(key) for key in cls.metadata_fields}
 
                     metadata = {
                         "observation_id": individual_id,
@@ -222,6 +226,10 @@ class DownloadINaturalist:
 
         with utils.data_directory(root):
             cls._download(force=force, **kwargs)
-        if hasattr(cls, 'summary') and 'licenses_url' in cls.summary and isinstance(cls.summary, str):
-            with open(os.path.join(root, cls.license_file_name), 'w') as file:
-                file.write(cls.summary['licenses_url'])
+        if (
+            hasattr(cls, "summary")
+            and "licenses_url" in cls.summary
+            and isinstance(cls.summary, str)
+        ):
+            with open(os.path.join(root, cls.license_file_name), "w") as file:
+                file.write(cls.summary["licenses_url"])
